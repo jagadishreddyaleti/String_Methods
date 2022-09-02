422770e70bc9cfa27579c3e716d414f6385a0a73
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-02 12:18:07 +0530

    Add files via upload

---
+++ String Methods part 2.py
@@ -0,0 +1,45 @@
+# """ 2nd september 2022 Friday
+# string methods"""
+ 
+# #  print('jagadish'.startswith('j'))
+# # print('jagadish'.startswith('k'))
+
+# # print('jagadish'.endswith('h'))
+# # print('jagadish'.endswith('j'))
+
+# print('jagadish reddy'.find('d'))
+# print('jagadish reddy'.find('r'))
+# print('jagadish reddy'.find('d',12))
+# print('jagadish reddy'.find('k'))
+# # print('jagadish reddy'.rfind('d'))
+# # print('jagadish reddy'.rfind('d',11))
+
+# a='python'
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+# print(a[5])
+# print(a[6])
+# print(a[7])
+
+# print('python'[0])
+# print('python'[1])
+# print('python'[2])
+# print('python'[3])
+# print('python'[4])
+# print('python'[5])
+# print('python'[6])
+# # print('python'[7])
+
+# print('september friday'.find('d'))
+# print('september friday'.index('d'))
+
+# print('september friday'.find('n'))
+# print('september friday'.index('n'))
+
+# print('september friday'.rindex('e'))
+# print('september friday'.index('e'))
+# print('september friday'.index('e',2))
+
