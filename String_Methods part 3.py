c18dbd57de5033426978d2d520dfb81bceaf8fe0
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-03 14:29:46 +0530

    Add files via upload

---
+++ String_Methods part 3.py
@@ -0,0 +1,30 @@
+""" 3rd september 2022 saturday 
+string methods part 3"""
+
+# print('hi'.zfill(1))
+# print('hi'.zfill(2))
+# print('hi'.zfill(3))
+# # print('hi'.zfill(4))
+
+# a,b=[(x) for x in input().split()]
+# print(a+b)
+# print(int(a)+int(b)) 
+# print(float(a)+flaot(b))
+# c=float(a)
+# d=float(b)
+# print(c+d)
+
+# print('the value of a is{a} and the value of b is{b}'.format(a=2,b=3)) 
+# print('the value of a is{a} and the value of b is{c}'.format(a=2,b=3)) 
+
+
+# print(len('python pro'.strip()))
+# print(len(' python pro '.strip()))
+# print(len(' python pro '.lstrip()))
+# print(len(' python pro '.rstrip()))
+# print(dir(str))
+
+print('python program'.replace('program','programming'))
+print('python program'.replace('progra','programming'))
+print('python program'.replace('pr','programming'))
+print('python program'.replace('p','programming'))
